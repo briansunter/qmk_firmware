ab0e8b2da2e045af94fe8304f4ef3734c0504835
Brian Sunter <brian@briansunter.com>
2020-06-21 11:56:25 -0700

    no transparent

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/default/keymap.c
@@ -27,27 +27,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6  ,                         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,
      KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                             _______,KC_PSCR,            _______, KC_P0,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______
+     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,XXXXXXX,KC_PLUS,
+     XXXXXXX,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
+     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                        XXXXXXX, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                                             XXXXXXX,KC_PSCR,            XXXXXXX, KC_P0,
+                                             XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,
+                                             XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,
+                                             XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX
   ),
 
   [_RAISE] = LAYOUT_6x6(
 
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        _______,_______,_______,_______,_______,KC_VOLU,
-       _______,_______,_______,_______,_______,_______,                        KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,KC_VOLD,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_LBRC,                        KC_RBRC,XXXXXXX,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
+       XXXXXXX,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_VOLU,
+       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                        KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,XXXXXXX,KC_VOLD,
+       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
 
-                                               _______,_______,            KC_EQL ,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______
+       XXXXXXX,XXXXXXX,            KC_EQL ,XXXXXXX,
+                                               XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,
+                                               XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,
+                                               XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX
   ),
 
 };
